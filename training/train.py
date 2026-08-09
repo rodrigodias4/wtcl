de98020659d2a4f70b7ee4a13ccc2db139a85507
Rodrigo Dias <rodrigofdias@tecnico.ulisboa.pt>
2026-08-09 16:29:20 +0100

    move functions that were only used in train..py from utils.py to that script

--- training/train.py
+++ training/train.py
@@ -45,8 +45,6 @@ from utils import (
     LEARNING_RATE_CRF_MULTIPLIER,
     LEARNING_RATE_FC_MULTIPLIER,
     MODEL_DEFAULT,
-    get_optimizer,
-    get_validation_debate,
     handle_interrupt,
     label2id,
     id2label,
@@ -401,6 +399,68 @@ def encode(
     return enc
 
 
+# Optimizer
+
+
+def decay_group(module):
+    return [
+        p
+        for n, p in module.named_parameters()
+        if p.requires_grad and "bias" not in n and "LayerNorm.weight" not in n
+    ]
+
+
+def no_decay_group(module):
+    return [
+        p
+        for n, p in module.named_parameters()
+        if p.requires_grad and ("bias" in n or "LayerNorm.weight" in n)
+    ]
+
+
+def get_optimizer(model, hparams):
+    optimizer_params = [
+        # Transformer
+        {
+            "params": decay_group(model.transformer),
+            "lr": hparams["lr"],
+            "weight_decay": hparams["weight_decay"],
+        },
+        {
+            "params": no_decay_group(model.transformer),
+            "lr": hparams["lr"],
+            "weight_decay": 0.0,
+        },
+        # FC layer
+        {
+            "params": decay_group(model.fc),
+            "lr": hparams["lr"] * hparams["lr_fc_mult"],
+            "weight_decay": hparams["weight_decay"],
+        },
+        {
+            "params": no_decay_group(model.fc),
+            "lr": hparams["lr"] * hparams["lr_fc_mult"],
+            "weight_decay": 0.0,
+        },
+    ]
+    if model.use_crf:
+        optimizer_params.extend(
+            [
+                {
+                    "params": decay_group(model.crf),
+                    "lr": hparams["lr"] * hparams["lr_crf_mult"],
+                    "weight_decay": 0.0,  # IMPORTANT: no decay on CRF
+                },
+                {
+                    "params": no_decay_group(model.crf),
+                    "lr": hparams["lr"] * hparams["lr_crf_mult"],
+                    "weight_decay": 0.0,
+                },
+            ]
+        )
+    return torch.optim.AdamW(optimizer_params)
+
+
 # --------------------------------
 # Dataset Definition
 # --------------------------------
@@ -450,6 +510,35 @@ class WTCLDataset(Dataset):
             "labels": labels,
             "crf_mask": crf_mask,
         }
+
+
+def get_validation_debate(df: pd.DataFrame, debates: list[str]) -> str:
+    """
+    Choosing validation debate that minimizes the distance to the mean
+    ratio of positive tokens across all debates.
+    This is a heuristic to select a validation debate that is representative of the overall dataset.
+    """
+    debate_ratios = {}
+    for debate in debates:
+        n_positive_chars = 0
+        n_tokens = 0
+        debate_data = df[df["debate_id"] == debate]
+
+        for index, row in debate_data.iterrows():
+            spans = eval(row["spans"])
+            for span in spans:
+                n_positive_chars += span["end"] - span["start"]
+            n_tokens += len(row["text"])
+
+        debate_ratios[debate] = n_positive_chars / n_tokens if n_tokens > 0 else 0
+
+    # Find the debate with the closest number of positive characters to the mean
+    mean_positive_chars = sum(debate_ratios.values()) / len(debate_ratios)
+    val_debate = min(
+        debate_ratios, key=lambda x: abs(debate_ratios[x] - mean_positive_chars)
+    )
+
+    return val_debate
 
 
 # --------------------------------
--- training/utils.py
+++ training/utils.py
@@ -18,10 +18,6 @@ from rich.progress import (
 
 MODEL_DEFAULT = "distilroberta-base"
 
-CRF_LEARNING_RATE_MULTIPLIER = 10
-DEBATE_TEMPERED_SAMPLING_ALPHA = 1.0  # 0: Debates are weighted equally / 1: Original
-BIO_TEMPERED_SAMPLING_ALPHA = 0.0  # 0: Original / 1: Linear weighting of BIO
-BIO_TEMPERED_SAMPLING_EPS = 1.0
 LEARNING_RATE_CRF_MULTIPLIER = 10.0
 LEARNING_RATE_FC_MULTIPLIER = 10.0
 
@@ -44,94 +40,6 @@ def handle_interrupt(signum, frame):
     torch.cuda.empty_cache()
 
     sys.exit(0)
-
-
-def get_validation_debate(df: pd.DataFrame, debates: list[str]) -> str:
-    """
-    Choosing validation debate that minimizes the distance to the mean
-    ratio of positive tokens across all debates.
-    This is a heuristic to select a validation debate that is representative of the overall dataset.
-    """
-    debate_ratios = {}
-    for debate in debates:
-        n_positive_chars = 0
-        n_tokens = 0
-        debate_data = df[df["debate_id"] == debate]
-
-        for index, row in debate_data.iterrows():
-            spans = eval(row["spans"])
-            for span in spans:
-                n_positive_chars += span["end"] - span["start"]
-            n_tokens += len(row["text"])
-
-        debate_ratios[debate] = n_positive_chars / n_tokens if n_tokens > 0 else 0
-
-    # Find the debate with the closest number of positive characters to the mean
-    mean_positive_chars = sum(debate_ratios.values()) / len(debate_ratios)
-    val_debate = min(
-        debate_ratios, key=lambda x: abs(debate_ratios[x] - mean_positive_chars)
-    )
-
-    return val_debate
-
-
-def decay_group(module):
-    return [
-        p
-        for n, p in module.named_parameters()
-        if p.requires_grad and "bias" not in n and "LayerNorm.weight" not in n
-    ]
-
-
-def no_decay_group(module):
-    return [
-        p
-        for n, p in module.named_parameters()
-        if p.requires_grad and ("bias" in n or "LayerNorm.weight" in n)
-    ]
-
-
-def get_optimizer(model, hparams):
-    optimizer_params = [
-        # Transformer
-        {
-            "params": decay_group(model.transformer),
-            "lr": hparams["lr"],
-            "weight_decay": hparams["weight_decay"],
-        },
-        {
-            "params": no_decay_group(model.transformer),
-            "lr": hparams["lr"],
-            "weight_decay": 0.0,
-        },
-        # FC layer
-        {
-            "params": decay_group(model.fc),
-            "lr": hparams["lr"] * hparams["lr_fc_mult"],
-            "weight_decay": hparams["weight_decay"],
-        },
-        {
-            "params": no_decay_group(model.fc),
-            "lr": hparams["lr"] * hparams["lr_fc_mult"],
-            "weight_decay": 0.0,
-        },
-    ]
-    if model.use_crf:
-        optimizer_params.extend(
-            [
-                {
-                    "params": decay_group(model.crf),
-                    "lr": hparams["lr"] * hparams["lr_crf_mult"],
-                    "weight_decay": 0.0,  # IMPORTANT: no decay on CRF
-                },
-                {
-                    "params": no_decay_group(model.crf),
-                    "lr": hparams["lr"] * hparams["lr_crf_mult"],
-                    "weight_decay": 0.0,
-                },
-            ]
-        )
-    return torch.optim.AdamW(optimizer_params)
 
 
 def create_progress_bar(console) -> Progress:
